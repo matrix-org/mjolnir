5b4c253002a602bbbbfc292fdfef470cb5b51eb8
Travis Ralston <travpc@gmail.com>
2020-02-13 13:43:30 -0700

    Add a 'set powerlevel' command
    Fixes https://github.com/matrix-org/mjolnir/issues/35

--- src/commands/CommandHandler.ts
+++ src/commands/CommandHandler.ts
@@ -33,6 +33,7 @@ import { execListProtectedRooms } from "./ListProtectedRoomsCommand";
 import { execAddProtectedRoom, execRemoveProtectedRoom } from "./AddRemoveProtectedRoomsCommand";
 import { execMoveAliasCommand } from "./MoveAliasCommand";
 import { execAddRoomToDirectoryCommand, execRemoveRoomFromDirectoryCommand } from "./AddRemoveRoomFromDirectoryCommand";
+import { execSetPowerLevelCommand } from "./SetPowerLevelCommand";
 
 export const COMMAND_PREFIX = "!mjolnir";
 
@@ -85,6 +86,8 @@ export async function handleCommand(roomId: string, event: any, mjolnir: Mjolnir
             return await execAddRoomToDirectoryCommand(roomId, event, mjolnir, parts);
         } else if (parts[1] === 'directory' && parts.length > 3 && parts[2] === 'remove') {
             return await execRemoveRoomFromDirectoryCommand(roomId, event, mjolnir, parts);
+        } else if (parts[1] === 'powerlevel' && parts.length > 3) {
+            return await execSetPowerLevelCommand(roomId, event, mjolnir, parts);
         } else {
             // Help menu
             const menu = "" +
@@ -111,6 +114,7 @@ export async function handleCommand(roomId: string, event: any, mjolnir: Mjolnir
                 "!mjolnir move <room alias> <room alias/ID>                          - Moves a <room alias> to a new <room ID>\n" +
                 "!mjolnir directory add <room alias/ID>                              - Publishes a room in the server's room directory\n" +
                 "!mjolnir directory remove <room alias/ID>                           - Removes a room from the server's room directory\n" +
+                "!mjolnir powerlevel <user ID> <power level> [room alias/ID]         - Sets the power level of the user in the specified room (or all protected rooms)\n" +
                 "!mjolnir help                                                       - This menu\n";
             const html = `<b>Mjolnir help:</b><br><pre><code>${htmlEscape(menu)}</code></pre>`;
             const text = `Mjolnir help:\n${menu}`;
---
+++ src/commands/SetPowerLevelCommand.ts
@@ -0,0 +1,40 @@
+/*
+Copyright 2020 The Matrix.org Foundation C.I.C.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import { Mjolnir } from "../Mjolnir";
+import { LogLevel, LogService } from "matrix-bot-sdk";
+import { logMessage } from "../LogProxy";
+
+// !mjolnir powerlevel <user ID> <level> [room]
+export async function execSetPowerLevelCommand(roomId: string, event: any, mjolnir: Mjolnir, parts: string[]) {
+    const victim = parts[2];
+    const level = Math.round(Number(parts[3]));
+    const inRoom = parts[4];
+
+    let targetRooms = inRoom ? [await mjolnir.client.resolveRoom(inRoom)] : Object.keys(mjolnir.protectedRooms);
+
+    for (const targetRoomId of targetRooms) {
+        try {
+            await mjolnir.client.setUserPowerLevel(victim, targetRoomId, level);
+        } catch (e) {
+            const message = e.message || (e.body ? e.body.error : '<no message>');
+            await logMessage(LogLevel.ERROR, "SetPowerLevelCommand", `Failed to set power level of ${victim} to ${level} in ${targetRoomId}: ${message}`);
+            LogService.error("SetPowerLevelCommand", e);
+        }
+    }
+
+    await mjolnir.client.unstableApis.addReactionToEvent(roomId, event['event_id'], '✅');
+}
