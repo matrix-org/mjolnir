457f0110bb22e594735e742a8f3b2eaf9089fede
David Teller <d.o.teller+github@gmail.com>
2021-04-16 15:29:03 +0200

    DSL: Cleanup

--- dsl.ts
+++ dsl.ts
@@ -1,7 +1,8 @@
 /**
- * A antispam rule.
+ * An elementary antispam rule, used to match a single value against a
+ * regexp (or an equivalent but more optimized substring search).
  * 
- * The rule will **reject** (i.e. consider as spam) if either `literals`
+ * The rule will **reject** (i.e. consider as spam) if either `substrings`
  * or `regexp` matches the value.
  *
  * The rule will **accept** (i.e. let pass) otherwise.
@@ -9,7 +10,7 @@
  * Note that this rule is typically obtained by or-ing several rules
  * on the ruleserver side.
  */
-type Rule = {
+type Matcher = {
     /// The date of the latest update.
     ///
     /// Used to avoid needlessly recompiling rules on the spamchecker side,
@@ -29,96 +30,89 @@ type Rule = {
 };
 
 /**
- * All the sources of information available to the antispam
- * when running `user_may_invite`.
+ * An antispam rule for `user_may_invite`.
  */
 type InviteRules = {
     /// The full user id of the inviter.
-    inviter_user_id: Rule,
+    inviter_user_id: Matcher,
 
     /// The full user id if the potential new room member.
-    new_member_user_id: Rule,
+    new_member_user_id: Matcher,
 
     /// The room id.
-    room_id: Rule,
+    room_id: Matcher,
 };
 
 /**
- * All the sources of information available to the antispam
- * when running `user_may_create_room`.
+ * An antispam rule for `user_may_create_room`.
  */
 type RoomCreateRules = {
     /// The full user id of the room creator.
-    user_id: Rule,
+    user_id: Matcher,
 };
 
 /**
- * All the sources of information available to the antispam
- * when running `user_may_create_room_alias`.
+ * An antispam rule for `user_may_create_room_alias`.
  */
 type AliasCreateRules = {
     /// The full user id of the alias creator.
-    user_id: Rule,
+    user_id: Matcher,
 
     /// The human-readable alias.
-    desired_alias: Rule,
+    desired_alias: Matcher,
 };
 
 /**
- * All the sources of information available to the antispam
- * when running `user_may_publish_room`.
+ * An antispam rule for `user_may_publish_room`.
  */
 type PublishRoomRules = {
     /// The full user id of the publisher.
-    publisher_user_id: Rule,
+    publisher_user_id: Matcher,
 
     /// The room id.
-    room_id: Rule,
+    room_id: Matcher,
 };
 
 /**
- * All the sources of information available to the antispam
- * when running `check_username_for_spam`.
+ * An antispam rule for `check_username_for_spam`.
  */
 type CheckUsernameForSpamRules = {
     /// The full user id of the user.
-    user_id: Rule,
+    user_id: Matcher,
 
     /// The display name of the user.
-    display_name: Rule,
+    display_name: Matcher,
 
     /// The URL towards the avatar of the user.
-    avatar_url: Rule,
+    avatar_url: Matcher,
 };
 
 /**
- * All the sources of information available to the antispam
- * when running `check_registration_for_spam`.
+ * An antispam rule for `check_registration_for_spam`.
  */
 type CheckRegistrationForSpamRules = {
     /// The username, if available.
-    maybe_user_name: Rule,
+    maybe_user_name: Matcher,
 
     /// The email used for registration, if available.
-    maybe_email: Rule,
+    maybe_email: Matcher,
 
     /// The list of user agents used during registration (possibly empty).
     /// A registration will be considered spammy if any of the
     /// user agents matches the rule.
-    user_agent_list: Rule,
+    user_agent: Matcher,
 
     /// The list of IPs used during registration (possibly empty).
     /// A registration will be considered spammy if any of the
     /// IPs matches the rule.
-    ip_list: Rule,
+    ip: Matcher,
 
     /// The auth provider, if available, e.g. "oidc", "saml", ...
-    maybe_auth_provider_id: Rule
+    maybe_auth_provider_id: Matcher
 };
 
 /**
- * All the sources of information available to the antispam
- * when running `check_event_for_spam`.
+ * An antispam rule for `check_event_for_spam`.
  */
 type CheckEventForSpamRules = {
     /// A path of fields within the event object.
@@ -127,31 +121,37 @@ type CheckEventForSpamRules = {
     ///
     /// If any of the fields is not present, the rule will **not** match (i.e. the
     /// message will not be considered spam).
-    event: { [path: string]: Rule },
+    event: { [path: string]: Matcher },
 };
 
 /**
- * A batch of rules.
+ * The complete list of rules for an instance of the spam-checker.
  */
 type RuleSet = {
+    /// If the rules match, user cannot invite other user.
     user_may_invite: InviteRules,
+
+    /// If the rules match, user cannot create room.
     user_may_create_room: RoomCreateRules,
+
+    /// If the rules match, user cannot create an alias for the room.
     user_may_create_room_alias: AliasCreateRules,
+
+    /// If the rules match, user cannot make the room public.
     user_may_publish_room: PublishRoomRules,
+
+    /// If the rules match, register user and immediately shadowban them.
+    check_registration_for_spam_shadowban: CheckRegistrationForSpamRules,
+
+    /// If the rules of `check_registration_for_spam_shadowban` do NOT
+    /// match but these rules match, deny registration.
+    check_registration_for_spam_deny: CheckRegistrationForSpamRules,
+
+    /// If the rules match, deny registration, regardless of
+    /// the result of `check_registration_for_spam_shadowban`
+    /// and `check_registration_for_spam_deny`.
     check_username_for_spam: CheckUsernameForSpamRules,
-    check_registration_for_spam: CheckRegistrationForSpamRules,
+
+    /// If the rules match, event will bounce.
     check_event_for_spam: CheckEventForSpamRules,
-};
-
-/**
- * An update sent by the rule server to the antispam.
- */
-type Update = {
-    /// A batch of rules to remove.
-    ///
-    /// Use special value `"*"` to clear out all rules.
-    remove: RuleSet | "*",
-
-    /// A batch of rules to add.
-    add: RuleSet,
 };
