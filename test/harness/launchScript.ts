57ef8ab7feb43601b12df33c9225d7d276fbbd0b
gnuxie <gnuxie@element.io>
2021-09-08 16:44:06 +0100

    Tidy up test harness.

--- test/harness/launchScript.ts
+++ test/harness/launchScript.ts
@@ -1,9 +1,7 @@
 import * as mjolnirTesting from './mjolnirTesting';
 
-console.log('wat')
 switch (process.argv[2]) {
     case 'up':
-        console.info('hmm')
         mjolnirTesting.upHarness();
         break;
     case 'down':
