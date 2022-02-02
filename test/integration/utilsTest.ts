dc0ccebbc395c0d1ec9affaad620d08ceb35f9ef
jesopo <github@lolnerd.net>
2022-02-02 13:18:02 +0000

    replaceRoomIdsWithPills integration test

---
+++ test/integration/utilsTest.ts
@@ -0,0 +1,31 @@
+import { strict as assert } from "assert";
+
+import { UserID } from "matrix-bot-sdk";
+import config from "../../src/config";
+import { replaceRoomIdsWithPills } from "../../src/utils";
+
+describe("Test: utils", function() {
+    it("replaceRoomIdsWithPills correctly turns a room ID in to a pill", async function() {
+        this.timeout(20000);
+
+        await this.mjolnir.client.sendStateEvent(
+            this.mjolnir.managementRoomId,
+            "m.room.canonical_alias",
+            "",
+            { alias: config.managementRoom }
+        );
+
+        const out = await replaceRoomIdsWithPills(
+            this.mjolnir.client,
+            `it's fun here in ${this.mjolnir.managementRoomId}`,
+            new Set([this.mjolnir.managementRoomId])
+        );
+
+        const ourHomeserver = new UserID(await this.mjolnir.client.getUserId()).domain;
+        assert.equal(
+            out.formatted_body,
+            `it's fun here in <a href="https://matrix.to/#/${config.managementRoom}?via=${ourHomeserver}">${config.managementRoom}</a>`
+        );
+    });
+});
+
