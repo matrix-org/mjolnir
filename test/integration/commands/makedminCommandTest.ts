1359ae859500e686c009ee473cbb26e9d32971a7
Marco Cirillo <maranda@lightwitch.org>
2022-02-07 14:45:12 +0100

    Add make admin command tests.

---
+++ test/integration/commands/makedminCommandTest.ts
@@ -0,0 +1,40 @@
+﻿import { strict as assert } from "assert";
+
+import config from "../../../src/config";
+import { newTestUser } from "../clientHelper";
+import { PowerLevelAction } from "matrix-bot-sdk";
+import { getFirstReaction } from "./commandUtils";
+
+describe("Test: The make admin command", function () {
+    // If a test has a timeout while awaitng on a promise then we never get given control back.
+    afterEach(function () { this.moderator ?.stop(); });
+
+    it('Mjölnir make the bot self room administrator and some other tester too', async function () {
+        this.timeout(60000);
+        const mjolnir = config.RUNTIME.client!
+        let mjolnirUserId = await mjolnir.getUserId();
+        let moderator = await newTestUser({ name: { contains: "moderator" } });
+        let tester = await newTestUser({ name: { contains: "tester" } });
+        let testerUserId = await tester.getUserId();
+        this.moderator = moderator;
+        this.tester = tester;
+        await moderator.joinRoom(config.managementRoom);
+        let targetRoom = await moderator.createRoom({ invite: [await moderator.getUserId(), mjolnirUserId, testerUserId] });
+        await moderator.sendMessage(this.mjolnir.managementRoomId, { msgtype: 'm.text.', body: `!mjolnir rooms add ${targetRoom}` });
+
+        try {
+            await moderator.start();
+            await getFirstReaction(moderator, this.mjolnir.managementRoomId, '✅', async () => {
+                return await moderator.sendMessage(this.mjolnir.managementRoomId, { msgtype: 'm.text.', body: `!mjolnir make admin ${targetRoom}` });
+            });
+            await getFirstReaction(moderator, this.mjolnir.managementRoomId, '✅', async () => {
+                return await moderator.sendMessage(this.mjolnir.managementRoomId, { msgtype: 'm.text.', body: `!mjolnir make admin ${targetRoom} ${tester.getUserId()}` });
+            });
+        } finally {
+            moderator.stop();
+        }
+
+        assert.ok(await mjolnir.userHasPowerLevelForAction(mjolnirUserId, targetRoom, PowerLevelAction.Ban), "Bot user is now room admin.");
+        assert.ok(await mjolnir.userHasPowerLevelForAction(testerUserId, targetRoom, PowerLevelAction.Ban), "Tester user is now room admin.");
+    });
+});
