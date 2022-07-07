b48904bc2b4fcd636176b12dbe173ad651930f23
gnuxie <gnuxie@element.io>
2022-07-07 13:07:27 +0100

    v1.5.0

--- synapse_antispam/setup.py
+++ synapse_antispam/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mjolnir",
-    version="1.4.2", # version automated in package.json - Do not edit this line, use `yarn version`.
+    version="1.5.0", # version automated in package.json - Do not edit this line, use `yarn version`.
     packages=find_packages(),
     description="Mjolnir Antispam",
     include_package_data=True,
