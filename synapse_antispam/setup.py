9e8546366a2b61e503470e479d4e5dd40d4d8532
H. Shay <hillerys@element.io>
2024-09-11 15:49:50 -0700

    v1.7.0

--- synapse_antispam/setup.py
+++ synapse_antispam/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mjolnir",
-    version="1.6.3", # version automated in package.json - Do not edit this line, use `yarn version`.
+    version="1.7.0", # version automated in package.json - Do not edit this line, use `yarn version`.
     packages=find_packages(),
     description="Mjolnir Antispam",
     include_package_data=True,
