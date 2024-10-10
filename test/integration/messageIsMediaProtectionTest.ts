10f49cd0335ee2cc56c8f9a5297da42c283fb2cf
H. Shay <hillerys@element.io>
2024-10-10 10:46:16 -0700

    lint

--- test/integration/messageIsMediaProtectionTest.ts
+++ test/integration/messageIsMediaProtectionTest.ts
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-
 import { MatrixClient } from "@vector-im/matrix-bot-sdk";
 import { newTestUser } from "./clientHelper";
 import { getFirstReaction } from "./commands/commandUtils";
