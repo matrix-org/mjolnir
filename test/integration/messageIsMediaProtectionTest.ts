1c84b0991c18f3bc0407778838aebcb15f96c30b
H. Shay <hillerys@element.io>
2024-10-10 10:41:13 -0700

    add copyright header

--- test/integration/messageIsMediaProtectionTest.ts
+++ test/integration/messageIsMediaProtectionTest.ts
@@ -1,3 +1,20 @@
+/*
+Copyright 2024 The Matrix.org Foundation C.I.C.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+
 import { MatrixClient } from "@vector-im/matrix-bot-sdk";
 import { newTestUser } from "./clientHelper";
 import { getFirstReaction } from "./commands/commandUtils";
@@ -48,7 +65,7 @@ describe("Test: Message is media", function () {
         let content = { msgtype: "m.image", body: "test.jpeg", url: mxc };
         let imageMessage = await client.sendMessage(testRoom, content);
 
-        let formatted_body = `<img src=${mxc} />`;
+        let formatted_body = `<img src="${mxc}" />`;
         let htmlContent = {
             msgtype: "m.image",
             body: formatted_body,
