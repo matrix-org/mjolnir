6a49ce302a3859661bfa08ed14645dbeb477667d
Shay <hillerys@element.io>
2025-05-02 11:51:22 -0700

    Update src/utils.ts
    Co-authored-by: Travis Ralston <travisr@matrix.org>

--- src/utils.ts
+++ src/utils.ts
@@ -612,9 +612,9 @@ export function findLink(str: string): boolean {
             "((\\d{1,3}\\.){3}\\d{1,3}))" + // validate OR ip (v4) address
             "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // validate port and path
             "(\\?[;&a-z\\d%_.~+=-]*)?" + // validate query string
-            "(\\#[-a-z\\d_]*)?$",
+            "(\\#[-a-z\\d_]*)?$", // validate fragment locator
         "i",
-    ); // validate fragment locator
+    );
     let isLink = false;
     const words = str.split(" ");
     for (const word of words) {
